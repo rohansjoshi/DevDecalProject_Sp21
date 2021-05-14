23102e3b77855c1fb7ceacd3f232f50610aa27a0
Rohan <rohanjoshi.berkeley.edu>
2021-05-13 21:19:19 -0700

    second commit

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,8 @@
+//var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var MerchToken = artifacts.require("./MerchToken.sol");
+
+
+module.exports = function(deployer) {
+  //deployer.deploy(SimpleStorage);
+  deployer.deploy(MerchToken);
+};
